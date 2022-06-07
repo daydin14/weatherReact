ebe4834d851f49fb6abf130376c11499a4e3d579
David Aydin <david.aydin6@gmail.com>
2022-06-07 15:09:00 -0400

    Created WeatherForecast component, updated app.js

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./styles.css";
+import Weather from "./weatherData";
 
 export default function App() {
+  const weather = Weather.map((ele, index) => {
+    return <WeatherForecast {...ele} key={index} />;
+  });
+
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1> 
+      <h1>My WeatherIcons App</h1>
     </div>
   );
 }

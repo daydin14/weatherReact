3e31b9fc7c46fa33b8c62549017873e1786e06c3
David Aydin <david.aydin6@gmail.com>
2022-06-07 14:59:29 -0400

    Weather Data

---
+++ weather/src/weatherData.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+        conditions: "day",
+        time: ""
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+        conditions: "",
+        time: "",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+        conditions: "",
+        time: "",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+        conditions: "",
+        time: "",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+        conditions: "",
+        time: "",
+    },
+];

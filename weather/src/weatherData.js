0a322942e6f24d445f40b29c569a71fb00508f72
David Aydin <david.aydin6@gmail.com>
2022-06-07 15:03:50 -0400

    Weather Data Update

--- weather/src/weatherData.js
+++ weather/src/weatherData.js
@@ -1,27 +1,27 @@
 export default [
-    {
-        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
-        conditions: "day",
-        time: ""
-    },
-    {
-        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
-        conditions: "",
-        time: "",
-    },
-    {
-        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
-        conditions: "",
-        time: "",
-    },
-    {
-        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
-        conditions: "",
-        time: "",
-    },
-    {
-        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
-        conditions: "",
-        time: "",
-    },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+    conditions: "clear",
+    time: "day",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+    conditions: "partly sunny",
+    time: "night",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+    conditions: "stormy",
+    time: "night",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+    conditions: "cloudy",
+    time: "day",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+    conditions: "cloudy",
+    time: "night",
+  },
 ];
